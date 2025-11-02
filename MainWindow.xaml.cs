b702a6ee78993885334b75b73e94e630f7a4adde
MuLin <ming439611@outlook.com>
2025-11-03 00:16:32 +0800

    INIT

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+using Microsoft.UI.Xaml;
+
+namespace PhotoPlay;
+
+public sealed partial class MainWindow : Microsoft.UI.Xaml.Window
+{
+	private readonly MainWindow _current;
+
+	private readonly Microsoft.UI.Windowing.AppWindow _appWindow;
+
+	private readonly Microsoft.UI.Windowing.OverlappedPresenter _overlappedPresenter;
+
+	public MainWindow()
+	{
+		InitializeComponent();
+
+		_current = this;
+
+		SystemBackdrop = new Microsoft.UI.Xaml.Media.MicaBackdrop { Kind = Microsoft.UI.Composition.SystemBackdrops.MicaKind.BaseAlt };
+
+		_appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(Microsoft.UI.Win32Interop.GetWindowIdFromWindow(WinRT.Interop.WindowNative.GetWindowHandle(_current)));
+
+		_overlappedPresenter = (Microsoft.UI.Windowing.OverlappedPresenter)_appWindow.Presenter;
+
+		_overlappedPresenter.SetBorderAndTitleBar(true, false);
+
+		ExtendsContentIntoTitleBar = true;
+
+		SetTitleBar(AppTitleBar);
+	}
+
+	private string GetAppTitleFromSystem()
+	{
+		string appTitle = Windows.ApplicationModel.Package.Current.DisplayName;
+		return appTitle;
+	}
+
+	private string GetVersionForBuild()
+	{
+		string name = Windows.ApplicationModel.Package.Current.PublisherDisplayName;
+	#if DEBUG
+		string build = "Debug";
+	#elif RELEASE
+			string build = "Release";
+	#endif
+		return $"{name} · {build}";
+	}
+
+	private void WindowsButton_OnClick(object sender, Microsoft.UI.Xaml.RoutedEventArgs routedEventArgs)
+	{
+		Microsoft.UI.Xaml.Controls.Button? senderButton = sender as Microsoft.UI.Xaml.Controls.Button;
+		string?                            Tag          = senderButton.Tag as string;
+
+		switch (Tag)
+		{
+			case "WindowClose": Close(); break;
+			case "WindowMinimise": _overlappedPresenter.Minimize(); break;
+			case "WindowMaximise" when _overlappedPresenter.State == Microsoft.UI.Windowing.OverlappedPresenterState.Restored: _overlappedPresenter.Maximize(); break;
+			case "WindowMaximise" when _overlappedPresenter.State == Microsoft.UI.Windowing.OverlappedPresenterState.Maximized: _overlappedPresenter.Restore(); break;
+			case "WindowTheme":
+				if (_current.Content is Microsoft.UI.Xaml.FrameworkElement rootElement) rootElement.RequestedTheme = (rootElement.ActualTheme == Microsoft.UI.Xaml.ElementTheme.Light ? Microsoft.UI.Xaml.ElementTheme.Dark : Microsoft.UI.Xaml.ElementTheme.Light);
+				break;
+		}
+	}
+
+	private void Block_OnLoaded(object sender, RoutedEventArgs e)
+	{
+		var textBlock = sender as Microsoft.UI.Xaml.Controls.TextBlock;
+		// 确保文本已渲染
+		textBlock.UpdateLayout();
+
+		// 获取 TextBlock 实际渲染后的宽度
+		double textBlockWidth  = textBlock.ActualWidth;
+		double textBlockHeight = textBlock.ActualHeight;
+
+		// 设置 Border 的宽度为 TextBlock 宽度 + 8
+		MyBorder.Width  = textBlockWidth + 8;
+		MyBorder.Height = textBlockHeight + 8;
+	}
+}
